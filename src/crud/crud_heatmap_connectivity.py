2c427ad167e30d3fbeb3277c8d31c42894d9b43f
Nihar Thakkar <nihartar@gmail.com>
2024-03-10 04:28:52 +0000

    Improve performance of connectivity-based heatmap computation

--- src/crud/crud_heatmap_connectivity.py
+++ src/crud/crud_heatmap_connectivity.py
@@ -10,7 +10,6 @@ from src.schemas.layer import IFeatureLayerToolCreate, FeatureGeometryType
 from src.schemas.toolbox_base import (
     DefaultResultLayerName,
 )
-from src.schemas.project import IFeatureStandardProjectRead
 
 
 class CRUDHeatmapConnectivityBase(CRUDHeatmapBase):
@@ -23,64 +22,26 @@ class CRUDHeatmapConnectivityBase(CRUDHeatmapBase):
     def __init__(self, job_id, background_tasks, async_session, user_id, project_id):
         super().__init__(job_id, background_tasks, async_session, user_id, project_id)
 
-    async def compute(
+    def build_query(
         self,
         params: IHeatmapConnectivityActive,
-        reference_area_layer: IFeatureStandardProjectRead,
+        reference_area_table: str,
         result_table: str,
         result_layer_id: str,
     ):
-        """Computes connectivity-based heatmap."""
+        """Builds SQL query to compute connectivity-based heatmap."""
 
-        # Fetch H3_3 cells within reference area (we run the computation cell-by-cell for performance reasons)
-        h3_3_cells = (await self.async_session.execute(
-            f"""
-                WITH reference_area AS (
-                    SELECT ST_Union(geom) AS geom
-                    FROM {reference_area_layer.table_name}
-                    WHERE {reference_area_layer.where_query}
-                )
-                SELECT h3_short
-                FROM reference_area ra,
-                LATERAL temporal.fill_polygon_h3_3(ra.geom);
-            """
-        )).fetchall()
-        h3_3_cells = [row[0] for row in h3_3_cells]
-
-        # Compute connectivity-based heatmap and write to result table
-        for h3_3 in h3_3_cells:
-            sql_compute_heatmap = f"""
-                WITH reference_area AS (
-                    SELECT ST_Union(geom) AS geom
-                    FROM {reference_area_layer.table_name}
-                    WHERE {reference_area_layer.where_query}
-                ),
-                origin_cells_h3_10 AS (
-                    SELECT h3_index
-                    FROM reference_area ra,
-                    LATERAL temporal.fill_polygon_h3_10(ra.geom)
-                ),
-                sub_matrix AS (
-                    SELECT matrix.orig_id, (ARRAY_LENGTH(matrix.dest_id, 1) * {self.H3_10_CELL_AREA_SQM}) AS area_sqm
-                    FROM origin_cells_h3_10 o, {self.TRAVELTIME_MATRIX_TABLE[params.routing_type]} matrix
-                    WHERE matrix.h3_3 = {h3_3}
-                    AND matrix.orig_id = o.h3_index
-                    AND matrix.traveltime <= {params.max_traveltime}
-                    UNION ALL
-                    SELECT h3_index AS orig_id, {self.H3_10_CELL_AREA_SQM} AS area_sqm
-                    FROM origin_cells_h3_10
-                ),
-                grouped AS (
-                    SELECT orig_id, SUM(area_sqm) AS accessibility
-                    FROM sub_matrix
-                    GROUP BY orig_id
-                )
-                INSERT INTO {result_table} (layer_id, geom, text_attr1, float_attr1)
-                SELECT '{result_layer_id}', ST_SetSRID(h3_cell_to_boundary(orig_id)::geometry, 4326),
-                    orig_id, accessibility
-                FROM grouped;
-            """
-            await self.async_session.execute(sql_compute_heatmap)
+        query = f"""
+            INSERT INTO {result_table} (layer_id, geom, text_attr1, float_attr1)
+            SELECT '{result_layer_id}', ST_SetSRID(h3_cell_to_boundary(matrix.orig_id)::geometry, 4326),
+                matrix.orig_id, SUM((ARRAY_LENGTH(matrix.dest_id, 1) * {self.H3_10_CELL_AREA_SQM}))
+            FROM {reference_area_table} o, {self.TRAVELTIME_MATRIX_TABLE[params.routing_type]} matrix
+            WHERE matrix.h3_3 = o.h3_3
+            AND matrix.orig_id = o.h3_index
+            AND matrix.traveltime <= {params.max_traveltime}
+            GROUP BY matrix.orig_id;
+        """
+        return query
 
 
 class CRUDHeatmapConnectivityActiveMobility(CRUDHeatmapConnectivityBase):
@@ -93,6 +54,9 @@ class CRUDHeatmapConnectivityActiveMobility(CRUDHeatmapConnectivityBase):
 
         # Fetch reference area table
         reference_area_layer = await self.get_layers_project(params)
+        reference_area_table = await self.create_table_polygons_to_h3_grid(
+            reference_area_layer["reference_area_layer_project_id"]
+        )
 
         # Initialize result table
         result_table = f"{settings.USER_DATA_SCHEMA}.{FeatureGeometryType.polygon.value}_{str(self.user_id).replace('-', '')}"
@@ -110,12 +74,12 @@ class CRUDHeatmapConnectivityActiveMobility(CRUDHeatmapConnectivityBase):
         )
 
         # Compute heatmap & write to output table
-        await self.compute(
+        await self.async_session.execute(self.build_query(
             params=params,
-            reference_area_layer=reference_area_layer["reference_area_layer_project_id"],
+            reference_area_table=reference_area_table,
             result_table=result_table,
             result_layer_id=str(layer_heatmap.id),
-        )
+        ))
 
         # Register feature layer
         await self.create_feature_layer_tool(
