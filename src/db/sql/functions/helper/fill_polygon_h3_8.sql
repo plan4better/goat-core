9037663a381e0faa4bc9fbcd21c19da0e3ad34f8
Nihar Thakkar <nihartar@gmail.com>
2024-04-24 20:37:47 +0000

    Update SQL functions

--- src/db/sql/functions/helper/fill_polygon_h3_8.sql
+++ src/db/sql/functions/helper/fill_polygon_h3_8.sql
@@ -1,7 +1,7 @@
 /*This function returns the h3 indexes that are intersecting the borderpoints of a specified geometry*/
-DROP FUNCTION IF EXISTS basic.fill_polygon_h3_6;
-CREATE OR REPLACE FUNCTION basic.fill_polygon_h3_6(geom geometry)
-RETURNS TABLE (h3_index h3index, h3_short int, h3_boundary geometry(linestring, 4326), h3_geom geometry(polygon, 4326))
+DROP FUNCTION IF EXISTS basic.fill_polygon_h3_8;
+CREATE OR REPLACE FUNCTION basic.fill_polygon_h3_8(geom geometry)
+RETURNS TABLE (h3_index h3index, h3_short bigint, h3_boundary geometry(linestring, 4326), h3_geom geometry(polygon, 4326))
 LANGUAGE plpgsql
 AS $function$
 BEGIN
@@ -16,13 +16,13 @@ BEGIN
     ),
     h3_ids AS
     (
-        SELECT h3_lat_lng_to_cell(b.geom, 6) h3_index
+        SELECT h3_lat_lng_to_cell(b.geom, 8) h3_index
         FROM border_points b
         UNION ALL
-        SELECT h3_polygon_to_cells(p.geom, ARRAY[]::polygon[], 6) h3_index
+        SELECT h3_polygon_to_cells(p.geom, ARRAY[]::polygon[], 8) h3_index
         FROM polygons p
     )
-    SELECT sub.h3_index, basic.to_short_h3_6(sub.h3_index::bigint) AS h3_short,
+    SELECT sub.h3_index, basic.to_short_h3_8(sub.h3_index::bigint) AS h3_short,
         ST_ExteriorRing(ST_SetSRID(geometry(h3_cell_to_boundary(sub.h3_index)), 4326)) as h3_boundary,
         ST_SetSRID(geometry(h3_cell_to_boundary(sub.h3_index)), 4326) as h3_geom
     FROM h3_ids sub
