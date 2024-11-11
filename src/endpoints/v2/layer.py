563ecf4dc205ebe297ef0533977415a641892194
majkshkurti <majkshkurti@outlook.com>
2024-11-12 00:56:30 +0100

    fix: update_layer_dataset

--- src/endpoints/v2/layer.py
+++ src/endpoints/v2/layer.py
@@ -555,6 +555,7 @@ async def update_layer_dataset(
         description=existing_layer.description,
         folder_id=existing_layer.folder_id,
         properties=existing_layer.properties,
+        other_properties=existing_layer.other_properties,
         dataset_id=dataset_id,
     )
     await CRUDLayerDatasetUpdate(
